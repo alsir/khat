922157c9a97de1a03d5a4ad96312db016dcb9200
alsir <41924833+alsir@users.noreply.github.com>
2021-09-13 23:28:13 +0200

    Add files via upload

---
+++ khotwa event/javascript/script.js
@@ -0,0 +1,42 @@
+let countDate = new Date('oct 2, 2021 00:00:00').getTime();
+
+
+function countDown(){
+
+    let now = new Date().getTime();
+	gap = countDate - now;
+
+    let seconds = 1000;
+    let minutes = seconds * 60;
+    let hours = minutes * 60;
+    let days = hours * 24;
+
+    let d = Math.floor(gap / (days));
+	let h = Math.floor((gap % (days)) / (hours));
+	let m = Math.floor((gap % (hours)) / (minutes));
+	let s = Math.floor((gap % (minutes)) / (seconds));
+
+    document.getElementById('days').innerText = d;
+    document.getElementById('hours').innerText = h;
+    document.getElementById('minutes').innerText = m;
+    document.getElementById('seconds').innerText = s;
+
+}
+
+setInterval(function(){
+    countDown()
+},1000);
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener('click', navToggle);
+
+function navToggle() {
+   navToggler.classList.toggle("active");
+   const nav = document.querySelector(".nav");
+   nav.classList.toggle("open");
+   if(nav.classList.contains("open")){
+     nav.style.maxHeight = nav.scrollHeight + "px";
+   }
+   else{
+     nav.removeAttribute("style");
+   }
+} 
